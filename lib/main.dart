a216387780e10f73ffe40ffd82d5cecb2973023e
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2023-07-28 23:41:34 +0200

    Made progress thanks to ChatGPT.

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class Timeline extends StatefulWidget {
+  @override
+  _TimelineState createState() => _TimelineState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _TimelineState extends State<Timeline> {
+  double _scale = 1.0;
+  double _previousScale = 1.0;
+  Offset _offset = Offset.zero;
 
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onScaleStart: (details) {
+      //   setState(() {
+      //     _previousScale = _scale;
+      //   });
+      // },
+      // onScaleUpdate: (details) {
+      //   setState(() {
+      //     _scale = _previousScale * details.scale;
+      //   });
+      // },
+      onDoubleTap: () {
+        setState(() {
+          _scale = 1.0;
+          _offset = Offset.zero;
+        });
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _offset += details.delta;
+        });
+      },
+      child: Transform(
+        transform: Matrix4.identity()
+          ..translate(_offset.dx, _offset.dy)
+          ..scale(_scale),
+        child: CustomPaint(
+          size: Size.infinite,
+          painter: TimelinePainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class TimelinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 2.0;
+
+    canvas.drawLine(
+      Offset(size.width / 2, 0),
+      Offset(size.width / 2, size.height),
+      paint,
+    );
+
+    for (int i = 0; i < size.height; i += 50) {
+      canvas.drawCircle(Offset(size.width / 2, i.toDouble()), 10.0, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Timeline For Humanity',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.blue,
-        primaryColor: Colors.white,
-        brightness: Brightness.light,
       ),
-      darkTheme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey.shade900,
-        primaryColor: Colors.black,
-        primarySwatch: Colors.grey,
-        colorScheme: const ColorScheme.dark(),
-        brightness: Brightness.dark,
-      ),
-      themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Timeline View'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class LinePainter extends CustomPainter {
-  final BuildContext context;
-  LinePainter(this.context);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawLine(
-      const Offset(0, 0),
-      Offset(size.width, 0),
-      Paint()
-        ..color = Theme.of(context).colorScheme.inverseSurface
-        ..strokeWidth = size.height
-        ..strokeCap = StrokeCap.round,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double lineX = 0;
-  double lineY = 0;
-  double _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Stack(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          // mainAxisAlignment: MainAxisAlignment.center,
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-              left: lineX,
-              top: lineY,
-              child: Draggable(
-                feedback: CustomPaint(
-                  size: Size(_counter * 10, 8),
-                  painter: LinePainter(context),
-                ),
-                onDragEnd: (details) {
-                  setState(() {
-                    lineX = details.offset.dx;
-                    lineY = details.offset.dy;
-                  });
-                },
-                childWhenDragging: Container(),
-                child: CustomPaint(
-                  size: Size(_counter * 10, 8),
-                  painter: LinePainter(context),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _incrementCounter,
-        tooltip: 'New dot',
-        label: const Text("New Dot"),
-        icon: const Icon(Icons.add),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
+      home: Scaffold(
+        body: Timeline(),
       ),
     );
   }
